49ffb52533934a4314f1e931c5c04b03379ec7c6
Eduardo Oliveira <eduardoykky@gmail.com>
2023-02-02 20:06:49 -0300

    Colocando em Prática 1.1

---
+++ 1.1.js
@@ -0,0 +1,17 @@
+var soma = 2 + 3
+console.log(soma);
+
+var subtracao = -10 - -5
+console.log(subtracao);
+
+var divisao = 80 / 3
+console.log(divisao);
+
+var divisaoResto = 80 % 3
+console.log(divisaoResto);
+
+soma += 5
+console.log(soma);
+
+soma -= 3
+console.log(soma);
