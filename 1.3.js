4060b1c936099577e025e82494e63bcae69c4221
Eduardo Oliveira <eduardoykky@gmail.com>
2023-02-02 21:19:02 -0300

    Colocando em Prática 1.3

---
+++ 1.3.js
@@ -0,0 +1,23 @@
+"se caminho 5km e corro 2km, passo o dia bem."
+
+if ("caminho_5km && corro_2km, passo_o_dia-bem.") {    
+}
+
+"se levanto cedo e antes das 8h, saio tranquilo."
+
+if ("levanto_cedo || antes_das_8h, saio_tranquilo.") {
+}
+
+"Se NÃO termino o trabalho ou NÃO tenho compromisso, saio tarde."
+
+if ("!termino_o_trabalho || !tenho_compromisso, saio_tarde.") {
+}
+////////////////////////////
+
+var num = num / num || num / 1 ? console.log("primo") : console.log("n primo")
+
+////////////////////////////
+
+var hora = 1
+var minuto = 0
+hora = hora > 0 ? minuto = hora * 60 : minuto = 60
